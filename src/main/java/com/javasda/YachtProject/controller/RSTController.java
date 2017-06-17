630a7ff9e0197d50686e5fc372f28e61e6cd8844
KubaGut <jakub_gutkowski@wp.pl>
2017-06-17 18:01:25 +0200

    Usuniecie importow

--- src/main/java/com/javasda/YachtProject/controller/RSTController.java
+++ src/main/java/com/javasda/YachtProject/controller/RSTController.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Action;
 
 @Controller
 @RequestMapping()
